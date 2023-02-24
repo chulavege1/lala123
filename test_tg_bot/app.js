a73684709146608f4541336ab4edc98f27abab2b
chulavege1 <vkoshelev24@gmail.com>
2023-02-24 21:19:44 +0200

    dddd

--- test_tg_bot/app.js
+++ test_tg_bot/app.js
@@ -37,6 +37,12 @@ app.get("/", function (req, res) {
 // cors
 app.use(cors())
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
+
 app.use('/', indexRouter);
 // app.use('/example/b', usersRouter);
 
