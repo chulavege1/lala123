4b8059a9d24039531fbd680dea9bfe065825ff70
chulavege1 <vkoshelev24@gmail.com>
2023-02-25 04:56:34 +0200

    dada

--- test_tg_bot/app.js
+++ test_tg_bot/app.js
@@ -37,12 +37,6 @@ app.get("/", function (req, res) {
 // cors
 app.use(cors())
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next()
-});
-
 app.use('/', indexRouter);
 // app.use('/example/b', usersRouter);
 
